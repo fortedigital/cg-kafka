a533d22e0ac6a32654ce674a59460d89a6443e83
Daud Mohamed <daud.mohamed@fortedigital.no>
2024-04-02 09:57:11 +0200

    Add starter code for kotlin, python and typescript for kafka game

--- workshop3/3 - Game/kafka-demo/src/main/kotlin/com/fortedigital/dto/AnswerDTO.kt
+++
@@ -1,7 +0,0 @@
-package com.fortedigital.dto
-
-import com.fortedigital.service.formats.Category
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class AnswerDTO(val category: Category, val score: Int, val questionId: String)
--- workshop3/3 - Game/kafka-demo/src/main/resources/db/migration/V002__create_question_table.sql
+++
@@ -1,7 +0,0 @@
-create table question (
-    id serial primary key,
-    question_id varchar(64) not null,
-    question varchar(256) not null,
-    category int not null,
-    created varchar(64)
-);
